65ea2609ec7a08f38894b1c58b1abd77f2f28dc1
dianannperla <waafirmelyza@hotmail.com>
2025-08-15 13:19:12 +0100

    feat: initial commit for src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-container>
+      <h1>Trang Web Thống Kê</h1>
+      <data-input></data-input>
+      <data-chart></data-chart>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import DataInput from './components/DataInput.vue';
+import DataChart from './components/DataChart.vue';
+
+export default {
+  components: {
+    DataInput,
+    DataChart
+  }
+}
+</script>
+
+<style>
+</style>
