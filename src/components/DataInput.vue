42c9f57b3cea1f805ea594f87d3ab4b33d991935
dianannperla <waafirmelyza@hotmail.com>
2025-08-15 13:19:16 +0100

    feat: initial commit for src/components/DataInput.vue

---
+++ src/components/DataInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card>
+    <v-card-title>Nhập Dữ Liệu</v-card-title>
+    <v-card-text>
+      <v-text-field v-model="inputData" label="Nhập dữ liệu"></v-text-field>
+      <v-btn @click="addData">Thêm</v-btn>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputData: '',
+    };
+  },
+  methods: {
+    addData() {
+      // Logic để thêm dữ liệu
+      this.inputData = '';
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
